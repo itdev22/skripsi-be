eec988072248ec3eaf74860534468eda38128a7c
Rama <90917112+itdev22@users.noreply.github.com>
2025-05-17 00:18:48 +0700

    customer installation

--- internal/api/admin/customer/installation/request.go
+++ internal/api/admin/customer/installation/request.go
@@ -4,7 +4,7 @@ type CreateAdminCustomerInstallationRequest struct {
 	CustomerId   string   `json:"customer_id" validate:"required"`
 	TechnicianId string   `json:"technician_id" validate:"required"`
 	Date         string   `json:"date" validate:"required"`
-	Description  string   `json:"description" validate:"required"`
+	Description  string   `json:"description"`
 	Images       []string `json:"images" validate:"required"`
 }
 
